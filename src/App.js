092e9afc1ba4a64ce927956a5688b789a138987b
usofallen <dballen@byu.edu>
2022-06-19 21:25:37 -0600

    functioning serverside searching!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const {search, animals} = useAnimalSearch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Animal Farm</h1>
+
+      <input
+        type="text"
+        placeholder="Search for animals"
+        onChange={(e)=> search(e.target.value)}
+        />
+
+      <ul>
+        {animals.map((animal) => (
+          <Animal key = {animal.id} {...animal} />
+        ))}
+
+        {animals.length === 0 && 'No animals found'}
+      </ul>
+    </main>
+  );
+}
+
+function useAnimalSearch() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery');
+    search(lastQuery);
+  }, []);
+
+  const search = async (q) => {
+    const response = await fetch(
+      'http://localhost:8080?' + new URLSearchParams({ q }),
+    );
+
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem('lastQuery', q);
+  };
+
+  return {search, animals};
+}
+
+function Animal ({ type, name, age}) {
+  return (
+    <li>
+      <strong>{type}</strong> {name} ({age} years old)
+    </li>
   );
 }
 
